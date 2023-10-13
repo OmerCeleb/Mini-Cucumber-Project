b49a76c945093c1c0ee6cfa0f3bd5fe0cec05f21
OmerCeleb <omer534@outlook.com>
2023-10-13 22:11:56 +0200

    Cucumber mini project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.setProperty("webDriver.chrome.driver", "C:\\Users\\yeniy\\OneDrive\\Skrivbord\\chromedriver-win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("http://www.youtube.com");
+    }
+}
